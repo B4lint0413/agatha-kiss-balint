84ae39a3e592cdd92c0f8656be8bbf5feb00dfcb
B4lint0413 <balu050413@gmail.com>
2024-11-25 12:47:00 +0000

    OrderController created routed

--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    //
+    public function show($novelid){
+        $orders = DB::table("orders")->where("novel_id", $novelid);
+        $novelTitle = DB::table("novels")->first("id", $novelid)->value('hungarian_title');
+        return view("order.show", [
+            "title" => $novelTitle." című könyv rendelései",
+            "orders" => $orders->get(),
+            "novelTitle" => $novelTitle,
+            "count" => $orders->count()
+        ]);
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\NovelController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/novels', [NovelController::class, 'index'])->name('novels.index');
 Route::get('/novels/{category}', [NovelController::class, 'list'])->where('category', '\b(marple|poirot|egyeb)\b')->name('novels.list');
 Route::get('/novels/{id}', [NovelController::class, 'show'])->whereNumber('id')->name('novels.show');
+Route::get('/orders/{novelid}', [OrderController::class, 'show'])->whereNumber('novelid')->name('orders.show');
